12b072f6865fb35722f6f5f70a095246f87b35ef
Paulo Costa <c.paulopcosta@gmail.com>
2021-02-24 16:03:51 -0500

    Fixed functionality if user wasn't playing a track, added recent tracks, added user playlists

--- grabData.js
+++ grabData.js
@@ -10,60 +10,133 @@ var spotifyApi = new SpotifyWebApi({
   clientSecret: '',
   redirectUri: 'http://localhost:8888/callback'
 });
-spotifyApi.setAccessToken('');
-spotifyApi.setRefreshToken('');
+spotifyApi.setAccessToken(token);
 
 (async () => {
-  const me = await spotifyApi.getMe();
-  const currentlyPlaying = await spotifyApi.getMyCurrentPlayingTrack();
-  const getTracks = await spotifyApi.getMyTopTracks();
-  const getArtists = await spotifyApi.getMyTopArtists();
-  const playbackState = await spotifyApi.getMyCurrentPlaybackState();
+  var me = await spotifyApi.getMe();
+  var playbackState = await spotifyApi.getMyCurrentPlaybackState()
+  var currentlyPlaying = await spotifyApi.getMyCurrentPlayingTrack();
+  if (playbackState.body && playbackState.body.is_playing) {
+      app.get('/about', (req, res) => {
+        res.write('<!doctype html><html lang="en">' +
+          '<meta charset="utf-8"><title>Your Spotify data</title>' +
+          '<style type="text/css">* {font-family: "Helvetica", sans-serif; color:#d8d8d8; font-weight: lighter;} .head {text-align: center;} img {height: 200px; width: 200px} body {background-color: #121212;} img {display: block; margin: 0 auto 0 auto;} .data {display: flex; flex-wrap: wrap; margin-left: 5vw;} .dataItem {width: 50%;}</style>' +
+          '<h1 class="head">Current user: <a href="' + me.body.uri + '" target="_blank">' + me.body.display_name + '</a></h1>' +
+          '<h2 class="head">Currently playing: <a href="' + currentlyPlaying.body.item.uri + '" target="_blank">' + currentlyPlaying.body.item.name + '</a> by ' + artistName + '</h2>' +
+          '<img src=' + albumCover + '>' +
+          '<div class="data">' +
+          '<div class="dataItem">' +
+          '<h2 class="dataHead">Top tracks (last 6 months)</h2>' +
+          '<ol>' +
+          '<li><a href="' + topTracks[0].uri + '">' + topTracks[0].name + '</a> by ' + topTracks[0].artists[0].name + '</li>' +
+          '<li><a href="' + topTracks[1].uri + '">' + topTracks[1].name + '</a> by ' + topTracks[1].artists[0].name + '</li>' +
+          '<li><a href="' + topTracks[2].uri + '">' + topTracks[2].name + '</a> by ' + topTracks[2].artists[0].name + '</li>' +
+          '<li><a href="' + topTracks[3].uri + '">' + topTracks[3].name + '</a> by ' + topTracks[3].artists[0].name + '</li>' +
+          '<li><a href="' + topTracks[4].uri + '">' + topTracks[4].name + '</a> by ' + topTracks[4].artists[0].name + '</li>' +
+          '</ol>' +
+          '</div>' +
+          '<div class="dataItem">' +
+          '<h2 class="dataHead">Top artists (last 6 months)</h2>' +
+          '<ol>' +
+          '<li><a href="' + topArtists[0].uri + '">' + topArtists[0].name + '</a></li>' +
+          '<li><a href="' + topArtists[1].uri + '">' + topArtists[1].name + '</a></li>' +
+          '<li><a href="' + topArtists[2].uri + '">' + topArtists[2].name + '</a></li>' +
+          '<li><a href="' + topArtists[3].uri + '">' + topArtists[3].name + '</a></li>' +
+          '<li><a href="' + topArtists[4].uri + '">' + topArtists[4].name + '</a></li>' +
+          '</ol>' +
+          '</div>' +
+          '<div class="dataItem">' +
+          '<h2 class="dataHead">Recently played tracks</h2>' +
+          '<ol>' +
+          '<li><a href="' + recentlyPlayedTracks[0].track.uri + '">' + recentlyPlayedTracks[0].track.name + '</a> by ' + recentlyPlayedTracks[0].track.artists[0].name + '</li>' +
+          '<li><a href="' + recentlyPlayedTracks[1].track.uri + '">' + recentlyPlayedTracks[1].track.name + '</a> by ' + recentlyPlayedTracks[1].track.artists[0].name + '</li>' +
+          '<li><a href="' + recentlyPlayedTracks[2].track.uri + '">' + recentlyPlayedTracks[2].track.name + '</a> by ' + recentlyPlayedTracks[2].track.artists[0].name + '</li>' +
+          '<li><a href="' + recentlyPlayedTracks[3].track.uri + '">' + recentlyPlayedTracks[3].track.name + '</a> by ' + recentlyPlayedTracks[3].track.artists[0].name + '</li>' +
+          '<li><a href="' + recentlyPlayedTracks[4].track.uri + '">' + recentlyPlayedTracks[4].track.name + '</a> by ' + recentlyPlayedTracks[4].track.artists[0].name + '</li>' +
+          '</ol>' +
+          '</div>' +
+          '<div class="dataItem">' +
+          '<h2 class="dataHead">Top user playlists</h2>' +
+          '<ol>' +
+          '<li><a href="' + getPlaylists.body.items[0].uri + '">' + getPlaylists.body.items[0].name + '</a></li>' +
+          '<li><a href="' + getPlaylists.body.items[1].uri + '">' + getPlaylists.body.items[1].name + '</a></li>' +
+          '<li><a href="' + getPlaylists.body.items[2].uri + '">' + getPlaylists.body.items[2].name + '</a></li>' +
+          '<li><a href="' + getPlaylists.body.items[3].uri + '">' + getPlaylists.body.items[3].name + '</a></li>' +
+          '<li><a href="' + getPlaylists.body.items[4].uri + '">' + getPlaylists.body.items[4].name + '</a></li>' +
+          '</ol>' +
+          '</div>' +
+          '</div>'
+          )
+        res.end();
+      });
+  }
+  else {
+      app.get('/about', (req, res) => {
+        res.write('<!doctype html><html lang="en">' +
+          '<meta charset="utf-8"><title>Your Spotify data</title>' +
+          '<style type="text/css">* {font-family: "Helvetica", sans-serif; color:#d8d8d8; font-weight: lighter;} .head {text-align: center;} img {height: 200px; width: 200px} body {background-color: #121212;} img {display: block; margin: 0 auto 0 auto;} .data {display: flex; flex-wrap: wrap; margin-left: 5vw;} .dataItem {width: 50%;} .notice {text-align: center;}</style>' +
+          '<h1 class="head">Current user: <a href="' + me.body.uri + '">' + me.body.display_name + '</a></h1>' +
+          '<h2 class="notice">User is not currently playing any tracks</h2>' +
+          '<div class="data">' +
+          '<div class="dataItem">' +
+          '<h2 class="dataHead">Top tracks (last 6 months)</h2>' +
+          '<ol>' +
+          '<li><a href="' + topTracks[0].uri + '">' + topTracks[0].name + '</a> by ' + topTracks[0].artists[0].name + '</li>' +
+          '<li><a href="' + topTracks[1].uri + '">' + topTracks[1].name + '</a> by ' + topTracks[1].artists[0].name + '</li>' +
+          '<li><a href="' + topTracks[2].uri + '">' + topTracks[2].name + '</a> by ' + topTracks[2].artists[0].name + '</li>' +
+          '<li><a href="' + topTracks[3].uri + '">' + topTracks[3].name + '</a> by ' + topTracks[3].artists[0].name + '</li>' +
+          '<li><a href="' + topTracks[4].uri + '">' + topTracks[4].name + '</a> by ' + topTracks[4].artists[0].name + '</li>' +
+          '</ol>' +
+          '</div>' +
+          '<div class="dataItem">' +
+          '<h2 class="dataHead">Top artists (last 6 months)</h2>' +
+          '<ol>' +
+          '<li><a href="' + topArtists[0].uri + '">' + topArtists[0].name + '</a></li>' +
+          '<li><a href="' + topArtists[1].uri + '">' + topArtists[1].name + '</a></li>' +
+          '<li><a href="' + topArtists[2].uri + '">' + topArtists[2].name + '</a></li>' +
+          '<li><a href="' + topArtists[3].uri + '">' + topArtists[3].name + '</a></li>' +
+          '<li><a href="' + topArtists[4].uri + '">' + topArtists[4].name + '</a></li>' +
+          '</ol>' +
+          '</div>' +
+          '<div class="dataItem">' +
+          '<h2 class="dataHead">Recently played tracks</h2>' +
+          '<ol>' +
+          '<li><a href="' + recentlyPlayedTracks[0].track.uri + '">' + recentlyPlayedTracks[0].track.name + '</a> by ' + recentlyPlayedTracks[0].track.artists[0].name + '</li>' +
+          '<li><a href="' + recentlyPlayedTracks[1].track.uri + '">' + recentlyPlayedTracks[1].track.name + '</a> by ' + recentlyPlayedTracks[1].track.artists[0].name + '</li>' +
+          '<li><a href="' + recentlyPlayedTracks[2].track.uri + '">' + recentlyPlayedTracks[2].track.name + '</a> by ' + recentlyPlayedTracks[2].track.artists[0].name + '</li>' +
+          '<li><a href="' + recentlyPlayedTracks[3].track.uri + '">' + recentlyPlayedTracks[3].track.name + '</a> by ' + recentlyPlayedTracks[3].track.artists[0].name + '</li>' +
+          '<li><a href="' + recentlyPlayedTracks[4].track.uri + '">' + recentlyPlayedTracks[4].track.name + '</a> by ' + recentlyPlayedTracks[4].track.artists[0].name + '</li>' +
+          '</ol>' +
+          '</div>' +
+          '<div class="dataItem">' +
+          '<h2 class="dataHead">Top user playlists</h2>' +
+          '<ol>' +
+          '<li><a href="' + getPlaylists.body.items[0].uri + '">' + getPlaylists.body.items[0].name + '</a></li>' +
+          '<li><a href="' + getPlaylists.body.items[1].uri + '">' + getPlaylists.body.items[1].name + '</a></li>' +
+          '<li><a href="' + getPlaylists.body.items[2].uri + '">' + getPlaylists.body.items[2].name + '</a></li>' +
+          '<li><a href="' + getPlaylists.body.items[3].uri + '">' + getPlaylists.body.items[3].name + '</a></li>' +
+          '<li><a href="' + getPlaylists.body.items[4].uri + '">' + getPlaylists.body.items[4].name + '</a></li>' +
+          '</ol>' +
+          '</div>' +
+          '</div>'
+        )
+        res.end();
+    });
+  }
+  var getTracks = await spotifyApi.getMyTopTracks();
+  var getArtists = await spotifyApi.getMyTopArtists();
+  var recentTracks = await spotifyApi.getMyRecentlyPlayedTracks();
+  var getPlaylists = await spotifyApi.getUserPlaylists(me.body.id);
+  var recentlyPlayedTracks = recentTracks.body.items;
   var topArtists = getArtists.body.items;
   var topTracks = getTracks.body.items;
   var albumArray = currentlyPlaying.body.item.album;
   var artistsArray = currentlyPlaying.body.item.artists;
-  artistName = artistsArray[0].name;
-  albumCover = albumArray.images[0].url;
-  app.get('/about', (req, res) => {
-    res.write('<!doctype html>\n<html lang="en">\n' +
-      '\n<meta charset="utf-8">\n<title>Your Spotify data</title>\n' +
-      '<style type="text/css">* {font-family:"Arial", sans-serif;color:#d8d8d8;} img {height:200px;width:200px} body {background-color:#121212;} h1, h2 {text-align:center;} ol {margin-left: 30vw;} img {display: block; margin: 0 auto 0 auto;}</style>\n' +
-      '\n\n<h1>Current user: ' + me.body.display_name + '</h1>\n' +
-      '<h2>Currently playing: ' + currentlyPlaying.body.item.name + ' by ' + artistName + '</h2>' +
-      '<img src=' + albumCover + '>\n' +
-      '<h2>Top tracks (last 6 months)</h2>\n' +
-      '<ol>' +
-      '<li>' + topTracks[0].name + ' by ' + topTracks[0].artists[0].name + '</li>' +
-      '<li>' + topTracks[1].name + ' by ' + topTracks[1].artists[0].name + '</li>' +
-      '<li>' + topTracks[2].name + ' by ' + topTracks[2].artists[0].name + '</li>' +
-      '<li>' + topTracks[3].name + ' by ' + topTracks[3].artists[0].name + '</li>' +
-      '<li>' + topTracks[4].name + ' by ' + topTracks[4].artists[0].name + '</li>' +
-      '</ol>' +
-      '<h2>Top artists (last 6 months)</h2>\n' +
-      '<ol>' +
-      '<li>' + topArtists[0].name + '</li>' +
-      '<li>' + topArtists[1].name + '</li>' +
-      '<li>' + topArtists[2].name + '</li>' +
-      '<li>' + topArtists[3].name + '</li>' +
-      '<li>' + topArtists[4].name + '</li>' +
-      '</ol>' +
-      '\n\n')
-    res.end();
-  });
+  var artistName = artistsArray[0].name;
+  var albumCover = albumArray.images[0].url;
 })().catch(e => {
   console.error(e);
 });
-
-spotifyApi.refreshAccessToken().then(
-  function(data) {
-    console.log('The access token has been refreshed!');
-    spotifyApi.setAccessToken(data.body['access_token']);
-  },
-  function(err) {
-    console.log('Could not refresh access token', err);
-  }
-);
 
 app.listen(8888, () =>
   console.log(
